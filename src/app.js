43f3a7299923283241e0b1a5ee3fad33c81f9677
Dominic Pettifer <sironfoot@gmail.com>
2011-10-25 22:08:39 +0100

    Basic HTML structure

--- src/app.js
+++ src/app.js
@@ -9,31 +9,37 @@ var app = module.exports = express.createServer();
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-  
-  app.register('.html', require('ejs'));
+app.configure(function() {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
+	
+	app.register('.html', require('ejs'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function() {
+  	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function() {
+  	app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index.html', {
-    title: 'Express'
-  });
+  	res.render('index.html', {
+    	title: 'Blogs'
+  	});
+});
+
+app.get('/blog/:id/:title', function(req, res) {
+	res.render('blogPost.html', {
+		title: req.params.title
+	});
 });
 
 app.listen(3000);
